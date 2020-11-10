d1145ddfe63f49006ce954947040acfc0f2a0c01
Jason <s947517134@163.com>
2020-11-10 10:01:16 +0800

    pom文件导入依赖，测试数据库连接

---
+++ src/main/java/org/uml/little_restaurant/LittleRestaurantApplication.java
@@ -0,0 +1,13 @@
+package org.uml.little_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleRestaurantApplication.class, args);
+    }
+
+}

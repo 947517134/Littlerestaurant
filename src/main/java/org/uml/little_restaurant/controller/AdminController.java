0ce3198c0243b6fd1adf550db9af9ea8405c1540
石业鹏 <846004828@qq.com>
2020-11-18 21:56:22 +0800

    新增管理员controller

---
+++ src/main/java/org/uml/little_restaurant/controller/AdminController.java
@@ -0,0 +1,13 @@
+package org.uml.little_restaurant.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 管理员专用 后台管理系统 各项功能均由调用Restaurant类完成
+ *
+ */
+@RestController
+public class AdminController {
+    // TODO
+}

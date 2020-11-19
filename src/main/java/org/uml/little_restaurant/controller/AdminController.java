902d96ca949cde920dd045bd004eb2e2eb528795
石业鹏 <846004828@qq.com>
2020-11-19 23:53:50 +0800

    管理员登录功能完成

--- src/main/java/org/uml/little_restaurant/controller/AdminController.java
+++ src/main/java/org/uml/little_restaurant/controller/AdminController.java
@@ -1,7 +1,15 @@
 package org.uml.little_restaurant.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.uml.little_restaurant.pojo.Admin;
+import org.uml.little_restaurant.service.AdminService;
+import org.uml.little_restaurant.service.RestaurantService;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 管理员专用 后台管理系统 各项功能均由调用Restaurant类完成
@@ -9,7 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class AdminController {
-    // TODO
 
-    // 3333
+    @Autowired
+    AdminService adminService;
+
+    //管理员登录
+    @PostMapping("/adminLogin")
+    public int adminLogin(@RequestParam("username") String username,
+                          @RequestParam("password") String password,
+                          HttpServletRequest request){
+        if(adminService.adminLogin(username,password)!=0) {
+            Admin admin = new Admin(username,password);
+            request.getSession().setAttribute("admin",admin);
+            return 200;
+        }
+        return 404;
+    }
 }
